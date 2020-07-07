477faea73ee76b3431775826e97ef975cbe17b7e
Miki Tebeka <miki.tebeka@gmail.com>
2020-07-07 11:12:36 +0300

    Ch06_solution recorded

---
+++ Ch06/solution/wc.py
@@ -0,0 +1,19 @@
+"""Print count of words (ignoring case)"""
+from collections import Counter
+import unicodedata
+
+
+def normalize(word):
+    """Normalize word"""
+    word = unicodedata.normalize('NFKC', word)
+    return word.casefold()
+
+
+wc = Counter()
+with open('words.txt') as fp:
+    for word in fp:
+        word = word.strip()
+        wc[normalize(word)] += 1
+
+for word, count in wc.most_common():
+    print(f'{word:<15}\t{count}')
