deeee1ee05f560d54c7731724f6dde43c49fc4f7
Miki Tebeka <miki.tebeka@gmail.com>
2020-07-07 11:12:36 +0300

    Ch06_challenge recorded

---
+++ Ch06/challenge/wc.py
@@ -0,0 +1,12 @@
+"""Print count of words (ignoring case)"""
+from collections import Counter
+
+
+wc = Counter()
+with open('words.txt') as fp:
+    for word in fp:
+        word = word.strip()
+        wc[word] += 1
+
+for word, count in wc.most_common():
+    print(f'{word:<15}\t{count}')
